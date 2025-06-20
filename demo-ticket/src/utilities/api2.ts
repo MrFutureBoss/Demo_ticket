83cc269d430b66cb690034a2b18fdf8aa478d577
MrFutureBoss <maitu10e2@gmail.com>
2025-06-20 08:35:35 +0700

    add server deloyment port for frontend

--- demo-ticket/src/utilities/api2.ts
+++ demo-ticket/src/utilities/api2.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api2 = axios.create({
-  baseURL: "http://localhost:9999/api",
+  //Dev port
+  // baseURL: "http://localhost:9999/api",
+  //Prod port
+  baseURL: "https://demo-ticket.onrender.com/api/",
 });
 
 export default api2;
